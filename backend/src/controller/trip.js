9f545e10552c455fb9eeb85f6c74205ca5e6487a
Komalasari076 <sitikomala076@gmail.com>
2025-03-12 17:01:53 +0700

    memperbarui data trip (link image)

--- backend/src/controller/trip.js
+++ backend/src/controller/trip.js
@@ -15,44 +15,10 @@ const TRIPS = [
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has been the industry's standard dummy text ever since the 1500s. When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.",
     image:
-      "https://tse1.mm.bing.net/th?id=OIP.x-OIRj6seBewJoR-v-nK1wHaE7&pid=Api",
+      "https://res.cloudinary.com/dmegegwqb/image/upload/v1638670749/trip-default-dewetour/sydney_p4d1w2.jpg",
   },
   {
     id: 2,
-    title: "5D/4N Amazing Japan Adventure",
-    country: "Japan",
-    accommodation: "Hotel 5 Stars",
-    transportation: "Japan Airlines",
-    eat: "Included",
-    durationDay: 5,
-    durationNight: 4,
-    date: "2025-07-10",
-    price: 15000000,
-    quota: "10/15",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has been the industry's standard dummy text ever since the 1500s. When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.",
-    image:
-      "https://tse2.mm.bing.net/th?id=OIP.YSQLs8xIiG-DS9Tc-_uqaAHaE8&pid=Api",
-  },
-  {
-    id: 3,
-    title: "4D/3N Romantic Paris Getaway",
-    country: "France",
-    accommodation: "Hotel 5 Stars",
-    transportation: "Air France",
-    eat: "Included",
-    durationDay: 4,
-    durationNight: 3,
-    date: "2025-08-05",
-    price: 17000000,
-    quota: "8/10",
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has been the industry's standard dummy text ever since the 1500s. When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.",
-    image:
-      "https://tse2.mm.bing.net/th?id=OIP.jq-7CHhNCoib0sAhjFXUlQHaE8&pid=Api",
-  },
-  {
-    id: 4,
     title: "7D/6N Breathtaking Swiss Alps",
     country: "Switzerland",
     accommodation: "Luxury Resort",
@@ -66,10 +32,10 @@ const TRIPS = [
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has been the industry's standard dummy text ever since the 1500s. When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.",
     image:
-      "https://tse2.mm.bing.net/th?id=OIP.jq-7CHhNCoib0sAhjFXUlQHaE8&pid=Api",
+      "https://img.okezone.com/content/2022/06/10/408/2609462/5-keuntungan-tinggal-di-swiss-destinasi-impian-pelancong-dunia-BVbt4GTRwB.jpg",
   },
   {
-    id: 5,
+    id: 3,
     title: "3D/2N Bali Relaxation Retreat",
     country: "Indonesia",
     accommodation: "Beachfront Villa",
@@ -83,7 +49,41 @@ const TRIPS = [
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has been the industry's standard dummy text ever since the 1500s. When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.",
     image:
-      "https://tse3.mm.bing.net/th?id=OIP.-ceHr06YG7obxqFaVfzvVwHaE8&pid=Api",
+      "https://www.pelago.com/img/destinations/bali/0619-0941_bali-xlarge.webp",
+  },
+  {
+    id: 4,
+    title: "5D/4N Amazing Japan Adventure",
+    country: "Japan",
+    accommodation: "Hotel 5 Stars",
+    transportation: "Japan Airlines",
+    eat: "Included",
+    durationDay: 5,
+    durationNight: 4,
+    date: "2025-07-10",
+    price: 15000000,
+    quota: "10/15",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has been the industry's standard dummy text ever since the 1500s. When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.",
+    image:
+      "https://www.state.gov/wp-content/uploads/2019/04/Japan-2107x1406.jpg",
+  },
+  {
+    id: 5,
+    title: "4D/3N Romantic Paris Getaway",
+    country: "France",
+    accommodation: "Hotel 5 Stars",
+    transportation: "Air France",
+    eat: "Included",
+    durationDay: 4,
+    durationNight: 3,
+    date: "2025-08-05",
+    price: 17000000,
+    quota: "8/10",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has been the industry's standard dummy text ever since the 1500s. When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.",
+    image:
+      "https://ik.imagekit.io/tvlk/blog/2024/04/shutterstock_667548661.jpg",
   },
   {
     id: 6,
@@ -100,7 +100,7 @@ const TRIPS = [
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has been the industry's standard dummy text ever since the 1500s. When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.",
     image:
-      "https://tse3.mm.bing.net/th?id=OIP.-ceHr06YG7obxqFaVfzvVwHaE8&pid=Api",
+      "https://asset.mansionglobal.com/editorial/dubai-the-hottest-luxury-market-in-the-world-is-getting-a-fresh-influx-of-sparkling-new-builds/assets/NIaw0AnN6P/vela-viento-4-3500x1969.jpg",
   },
 ];
 
